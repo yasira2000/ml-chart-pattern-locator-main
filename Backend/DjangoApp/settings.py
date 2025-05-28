39ce5087122a21634630221b67ca2780f36f06fe
yasira2000 <yasirapunsith@gmail.com>
2025-05-28 08:02:22 +0530

    Fix cors issue

--- Backend/DjangoApp/settings.py
+++ Backend/DjangoApp/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api_v1',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

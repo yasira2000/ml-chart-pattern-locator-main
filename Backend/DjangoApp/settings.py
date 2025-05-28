1230e185ccb6c37bf81dbfa044bac0b5d642ec18
yasira2000 <yasirapunsith@gmail.com>
2025-05-28 07:38:43 +0530

    Add STATIC_ROOT to settings.py

--- Backend/DjangoApp/settings.py
+++ Backend/DjangoApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
